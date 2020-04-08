7d6680c647a27e668cc1df476ee368bd086aea02
Jenelle Langford <jenellelangford@Jenelles-Air.home>
2020-04-08 18:01:18 -0500

    Update.

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Build out all appropriate HTML tags
+// 2. List all variables(HTML tags) needed to work
+// 3. Set all questions and answers in arrays
+// 4. Add event listener for the "Start quiz" button
+// 5. Create function for timer to begin when "Start quiz" button is clicked
+// 6. Create loop that will run through 4 different questions and prompt "Right" or "Wrong" when answered. (If/Else statments)
+// 7. Call out all functions 
